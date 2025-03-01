70c8a2b4d7f8b8cb00a4f4612f6e1ee70e1dac01
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-03-01 13:11:58 -0600

    Style was added to the items in each section

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+// Espera a que se cargue el DOM y ya despues que se cargue, dispara el listener
 document.addEventListener('DOMContentLoaded', () => {
+    //Selecciona todos los botones que tienen la clase .add-btn
     const addButtons = document.querySelectorAll('.add-btn');
 
     addButtons.forEach(button => {
+        // Se
         button.addEventListener('click', () => {
             const inputBox = document.createElement('div');
             inputBox.innerHTML = `
-                <input type="text" class="task-input" placeholder="Enter task">
+                <input type="text" 
+                class="task-input" 
+                placeholder="Enter task"
+                >
+
                 <button class="check-btn">✔</button>
             `;
-            button.parentElement.appendChild(inputBox);
+            
+            const listContainer = button.closest('.dash-section').querySelector('.list-actv');
+            listContainer.appendChild(inputBox);
 
             const checkButton = inputBox.querySelector('.check-btn');
             checkButton.addEventListener('click', () => {
@@ -16,10 +25,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (taskInput.value.trim() !== '') {
                     const taskItem = document.createElement('li');
                     taskItem.textContent = taskInput.value;
-                    button.parentElement.nextElementSibling.querySelector('.activities').appendChild(taskItem);
+
+                    // Obtener quien es la clase padre
+                    const clasePadre = inputBox.parentElement.classList;
+
+                    if(clasePadre.contains("not-list")){
+                        taskItem.classList.add("not-item")
+                    } else if (clasePadre.contains("progress-list")){
+                        taskItem.classList.add("progress-item")
+                    } else {
+                        taskItem.classList.add("completed-item")
+                    }
+
+                    const taskList = button.closest('.dash-section').querySelector('.activities');
+                    taskList.appendChild(taskItem)
+
                     inputBox.remove();
                 }
             });
         });
     });
 });
+
+
+
